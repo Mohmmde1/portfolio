9c6f3b1960bba6c5b2a5c4e9ff49d3a006b94300
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-05-09 21:46:39 +0800

    bugfix: comply with react/no-unescaped-entities

--- src/app/page.js
+++ src/app/page.js
@@ -214,9 +214,9 @@ export default function Component() {
         <section className="w-full py-12 md:py-24 lg:py-32 border-t" id="contact">
           <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
             <div className="space-y-3">
-              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">Let's Connect</h2>
+              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">Let&apos;s Connect</h2>
               <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                I'm eager to discuss potential opportunities and how I can contribute to your team. Feel free to reach
+                I&apos;m eager to discuss potential opportunities and how I can contribute to your team. Feel free to reach
                 out to me.
               </p>
             </div>
